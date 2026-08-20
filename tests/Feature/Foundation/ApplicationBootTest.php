d0596fa2e8369e887e284896277b092632e6a50d
Richard Sparrow-Steels <richard.sparrow@hotmail.co.uk>
2026-08-21 00:03:00 +0100

    test: migrate homepage smoke tests

--- tests/Feature/Foundation/ApplicationBootTest.php
+++ tests/Feature/Foundation/ApplicationBootTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature\Foundation;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 final class ApplicationBootTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_application_boots_and_home_route_responds(): void
     {
         $this->get('/')->assertSuccessful();
